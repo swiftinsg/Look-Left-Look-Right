9bfd150e2ba8d3a29188aec0399b5f106572a627
Jia Chen <github@jiachen.app>
2025-05-04 15:06:02 +0800

    Initial Commit

---
+++ Look Left Look Right/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Look Left Look Right
+//
+//  Created by Jia Chen Yee on 5/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Look Left Look Right/Look_Left_Look_RightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Look_Left_Look_RightApp.swift
+//  Look Left Look Right
+//
+//  Created by Jia Chen Yee on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Look_Left_Look_RightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
