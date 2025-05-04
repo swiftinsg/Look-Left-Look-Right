715392ffb18fd457412c22a1a5c9dfbe2c502b83
Jia Chen <github@jiachen.app>
2025-05-05 02:12:19 +0800

    feat: create camera target

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Jia Chen Yee on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
